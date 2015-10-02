ab14e3c7694982766f663523d4420e01b21b8b0d
Matt Thomson <mattthomson@gocardless.com>
2015-10-02 12:39:52 +0100

    Load environment variables from config

--- server/src/main/java/com/gocardless/YouGotPaidApplication.java
+++ server/src/main/java/com/gocardless/YouGotPaidApplication.java
@@ -2,9 +2,21 @@ package com.gocardless;
 
 import com.gocardless.resources.WebhookResource;
 import io.dropwizard.Application;
+import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
+import io.dropwizard.configuration.SubstitutingSourceProvider;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class YouGotPaidApplication extends Application<YouGotPaidConfiguration> {
+    @Override
+    public void initialize(Bootstrap<YouGotPaidConfiguration> bootstrap) {
+        bootstrap.setConfigurationSourceProvider(
+                new SubstitutingSourceProvider(bootstrap.getConfigurationSourceProvider(),
+                        new EnvironmentVariableSubstitutor(false)
+                )
+        );
+    }
+
     @Override
     public void run(YouGotPaidConfiguration configuration, Environment environment) throws Exception {
         environment.jersey().register(new WebhookResource());
